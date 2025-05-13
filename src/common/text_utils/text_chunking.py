6114f5186b34e4b47e7e53d94f47c89447b10e85
Jakub Kowalewski <kowalewskijakub@icloud.com>
2025-05-13 13:40:47 +0200

    Zaktualizowano wzorzec artykułów, aby lepiej obsługiwał oznaczenia w nawiasach.

--- src/common/text_utils/text_chunking.py
+++ src/common/text_utils/text_chunking.py
@@ -9,7 +9,7 @@ def chunk_by_articles(text: str) -> list[str]:
     :return: Lista artykułów
     """
     articles = []
-    article_pattern = r"Art\. \d{1,}(\w{0,4})(\p{No}{0,2})\."
+    article_pattern = r"Art\. \d+(\w{0,4})(\s+\(\w*\)\s+)?\."
     ustawa_pattern = r"USTAWA.?z.?dnia.?\d{1,2}\s\w*\s\d{4}.r\."
 
     # Pomiń wszystko, co znajduje się przed nazwą ustawy
